1ae64cd8c928bf1fdfe7540aaef9fef97d527e29
欧桐江 <3210963178@qq.com>
2022-06-28 13:32:41 +0800

    学生管理系统初始化

---
+++ src/main/java/person/otj/school/config/WebMvcConfig.java
@@ -0,0 +1,10 @@
+package person.otj.school.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+}
---
+++ src/main/java/person/otj/school/controller/UserController.java
@@ -0,0 +1,13 @@
+package person.otj.school.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@Controller
+public class UserController {
+    @RequestMapping(value = {"/","/login"},method = {RequestMethod.POST,RequestMethod.GET})
+    public String login(){
+        return "login";
+    }
+
+
+}
