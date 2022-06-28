4a06707feeb84ddb7c500d7d64932fe980b7e8ab
欧桐江 <3210963178@qq.com>
2022-06-28 18:44:10 +0800

    6-28

--- src/main/java/person/otj/school/controller/PageController.java
+++ src/main/java/person/otj/school/controller/PageController.java
@@ -1,12 +1,14 @@
 package person.otj.school.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
-    @RequestMapping("/register")
+    @PostMapping("/register")
     public String register(){
+        System.out.println("进入register....");
         return "register";
     }
 }
--- src/main/java/person/otj/school/controller/UserController.java
+++ src/main/java/person/otj/school/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import person.otj.school.model.User;
+import person.otj.school.service.StudentService;
 import person.otj.school.service.UserService;
 
 @Controller
@@ -14,6 +15,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private StudentService studentService;
 
     @RequestMapping(value = {"/","/login"},method = {RequestMethod.POST,RequestMethod.GET})
     public String login(){
@@ -25,7 +29,7 @@ public class UserController {
         if(userService.findUserByUnamePwd(user.getUsername(),user.getPassword())==1){
             modelAndView.addObject(user);
             modelAndView.setViewName("studentList");
-
+            modelAndView.addObject("studentList",studentService.findAll());
         }else
         {
             modelAndView.setViewName("login");
